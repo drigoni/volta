611fbddc3c10dd0fc5b421864c5e4cc3c3c5ab6b
drigoni <davider1994@gmail.com>
2023-03-11 13:44:04 +0100

    Add code

--- examples/ctrl_uniter/concap/train.sh
+++ examples/ctrl_uniter/concap/train.sh
@@ -8,7 +8,7 @@ MODEL_CONFIG=ctrl_uniter_base
 OUTPUT_DIR=checkpoints/conceptual_captions/${MODEL}
 LOGGING_DIR=logs/conceptual_captions
 
-python train_concap.py \
+torchrun train_concap.py \
   --config_file config/${MODEL_CONFIG}.json \
   --train_batch_size 256 --gradient_accumulation_steps 1 --max_seq_length 38 \
   --learning_rate 1e-4 --adam_epsilon 1e-6 --adam_betas 0.9 0.999 --weight_decay 0.01 --warmup_proportion 0.1 --clip_grad_norm 5.0 \
@@ -17,5 +17,7 @@ python train_concap.py \
   --output_dir ${OUTPUT_DIR} \
   --logdir ${LOGGING_DIR} \
   --num_train_epochs 10 \
+  --distributed \
+  --local_rank 1 \
   --seed 0 \
 #  --resume_file ${OUTPUT_DIR}/${MODEL_CONFIG}/pytorch_ckpt_latest.tar
